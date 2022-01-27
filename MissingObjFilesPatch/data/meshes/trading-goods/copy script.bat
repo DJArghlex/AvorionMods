9fa4b21a10af7b17fe556eaa10b5e1d34897ec95
rglx <1321984+rglx@users.noreply.github.com>
2022-01-27 07:52:59 -0800

    add the (atm game-breaking) missing goods models patchmod
    currently doesn't work because the game's OBJ reader is broken and just crashes the client if it tries to load an OBJ.

---
+++ MissingObjFilesPatch/data/meshes/trading-goods/copy script.bat
@@ -0,0 +1,92 @@
+copy sphere.obj accelerator.obj
+copy sphere.mtl accelerator.mtl
+
+copy sphere.obj acid.obj
+copy sphere.mtl acid.mtl
+
+copy sphere.obj adhesive-solvent.obj
+copy sphere.mtl adhesive-solvent.mtl
+
+copy sphere.obj crate-01.obj
+copy sphere.mtl crate-01.mtl
+
+copy sphere.obj crate-03.obj
+copy sphere.mtl crate-03.mtl
+
+copy sphere.obj crate-04.obj
+copy sphere.mtl crate-04.mtl
+
+copy sphere.obj crate-05.obj
+copy sphere.mtl crate-05.mtl
+
+copy sphere.obj crate-06.obj
+copy sphere.mtl crate-06.mtl
+
+copy sphere.obj crate-07.obj
+copy sphere.mtl crate-07.mtl
+
+copy sphere.obj crate-08.obj
+copy sphere.mtl crate-08.mtl
+
+copy sphere.obj crate-09.obj
+copy sphere.mtl crate-09.mtl
+
+copy sphere.obj crate-food.obj
+copy sphere.mtl crate-food.mtl
+
+copy sphere.obj crate-gas.obj
+copy sphere.mtl crate-gas.mtl
+
+copy sphere.obj crate-liquids.obj
+copy sphere.mtl crate-liquids.mtl
+
+copy sphere.obj crate-tools.obj
+copy sphere.mtl crate-tools.mtl
+
+copy sphere.obj crystal.obj
+copy sphere.mtl crystal.mtl
+
+copy sphere.obj diamond.obj
+copy sphere.mtl diamond.mtl
+
+copy sphere.obj display.obj
+copy sphere.mtl display.mtl
+
+copy sphere.obj energy-cell.obj
+copy sphere.mtl energy-cell.mtl
+
+copy sphere.obj gem.obj
+copy sphere.mtl gem.mtl
+
+copy sphere.obj generator.obj
+copy sphere.mtl generator.mtl
+
+copy sphere.obj medical-supplies.obj
+copy sphere.mtl medical-supplies.mtl
+
+copy sphere.obj metal-plate.obj
+copy sphere.mtl metal-plate.mtl
+
+copy sphere.obj plasma-cell.obj
+copy sphere.mtl plasma-cell.mtl
+
+copy sphere.obj satellite.obj
+copy sphere.mtl satellite.mtl
+
+copy sphere.obj silicon.obj
+copy sphere.mtl silicon.mtl
+
+copy sphere.obj solar-cell.obj
+copy sphere.mtl solar-cell.mtl
+
+copy sphere.obj solarpanel.obj
+copy sphere.mtl solarpanel.mtl
+
+copy sphere.obj steel-tube.obj
+copy sphere.mtl steel-tube.mtl
+
+copy sphere.obj tesla-coil.obj
+copy sphere.mtl tesla-coil.mtl
+
+copy sphere.obj toxic-waste.obj
+copy sphere.mtl toxic-waste.mtl
