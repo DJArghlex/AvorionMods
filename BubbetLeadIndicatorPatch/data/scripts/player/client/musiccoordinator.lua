7bdb4329136155d1d81d9a5d8e0737fe72e4755f
rglx <1321984+rglx@users.noreply.github.com>
2023-12-27 18:27:58 -0500

    fix lead indicators patchmod to prevent clobbering

--- BubbetLeadIndicatorPatch/data/scripts/player/client/musiccoordinator.lua
+++ BubbetLeadIndicatorPatch/data/scripts/player/client/musiccoordinator.lua
@@ -7,12 +7,33 @@ if onClient() then
 local stashedMusicCoordinator_initialize = MusicCoordinator.initialize
 function MusicCoordinator.initialize(...)
 	stashedMusicCoordinator_initialize(...)
-	print("rglx_LeadIndicatorPatch: shimming musiccoordinator to load...")
+	print("rglx_BrakingDisplayClientShim: shimming musiccoordinator to load...")
 	-- load our scripts jankily
-	LeadIndicator = include("player/ui/leadindicator") -- include into our array
-	LeadIndicator.copyIntoOtherNamespace(MusicCoordinator) -- copy our stuff into the new namespace & reinitialize
+
+	-- don't do anything if the server's already loaded the script onto us
+	for id,scriptname in ipairs(Player():getScripts()) do
+		if scriptname == "data/scripts/player/ui/leadindicator.lua" then
+			return
+		end
+	end
+
+	-- read and execute our script- it should return its functions and variables
+	LeadIndicator = include("player/client/braking_distance_display") -- include into our array
+
+	-- stash a function if existing
+	if MusicCoordinator.onPostRenderHud then
+		if LeadIndicator.onPostRenderHud then
+			print("rglx_BrakingDisplayClientShim: shimming onPostRenderHud()...")
+			LeadIndicator.stash_onPostRenderHud = MusicCoordinator.onPostRenderHud -- stash extant code 
+			MusicCoordinator.onPostRenderHud = function(...) LeadIndicator.stash_onPostRenderHud(...); LeadIndicator.onPostRenderHud(...); end -- clobber with a new function that runs the old code and our new code
+		end
+	else
+		print("rglx_BrakingDisplayClientShim: clobber onPostRenderHud()...")
+		MusicCoordinator.onPostRenderHud = LeadIndicator.onPostRenderHud -- no existing code is present, we can simply just add our code.
+	end
+
+	LeadIndicator.initialize(...) -- run our initialization function
+
 end
 
-else
-	print("rglx_LeadIndicatorPatch: loaded onto server- this won't do anything")
 end
